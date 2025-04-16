5454ca8c822a134f1d99e2ef604227aabcc1c307
Mig1881 <miguelrubioibor@gmail.com>
2025-04-16 21:07:55 +0200

    Metrics with prometheus and grafana

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,8 @@ const products = require('./route/products');
 const suppliers = require('./route/suppliers');
 const users = require('./route/users');
 const { config } = require('./config/configuration');
+const promClient = require('prom-client');
+const { httpRequestDurationSeconds, httpRequestsTotal, inFlightRequests } = require('./config/metrics');
 
 //*** INICIO DE LA APLICACION */
 const IMAGES_PATH = './images/';
@@ -15,6 +17,49 @@ app.use(cors());
 app.use(express.json());
 // La carpeta de las imágenes se sirve estáticamente (http://localhost:8081/xxxxxxxx.jpg)
 app.use(express.static(IMAGES_PATH))
+
+// Recoge métricas por defecto
+promClient.collectDefaultMetrics();
+// Recoger las métricas configuradas en config/metrics
+app.use((req, res, next) => {
+    // Anota la request en vuelo
+    inFlightRequests.inc();
+
+    // Inicia un timer para comenzar a calcular la duración de la request
+    const end = httpRequestDurationSeconds.startTimer();
+
+    const method = req.method;
+    const path = req.route ? req.route.path : req.path; 
+
+    // Cada vez que una request termina, se recogen las métricas configuradas
+    res.on('finish', () => {
+        const statusCode = res.statusCode;
+
+        // Captura la duración de la petición
+        end({ method, path, code: statusCode }); 
+        // Incrementa el contador de peticiones HTTP
+        httpRequestsTotal.inc({
+            code: statusCode,
+            method: method.toLowerCase(),
+            path: path
+        });
+
+        // Decrementa el contador de peticiones en vuelo
+        inFlightRequests.dec();
+    });
+
+    next();
+});
+
+// Define el endpoint que usará prometheus para recoger las métricas
+app.get('/metrics', async(req, res) => {
+    try {
+        res.set('Content-Type', promClient.register.contentType);
+        res.end(await promClient.register.metrics());
+    } catch (error) {
+        res.status(500).end(error);
+    }
+});
 
 //USO de todas las operaciones
 app.use('/', products);
